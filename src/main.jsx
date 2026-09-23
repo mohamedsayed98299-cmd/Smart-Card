687855d5f49d396b2d8bf16e64f1e6809818cf94
mohamedsayed98299-cmd <mohamedsayed98299@gmail.com>
2026-09-23 04:02:06 +0300

    Fix GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Smart-Card">
       <App />
     </BrowserRouter>
   </React.StrictMode>
